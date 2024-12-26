7441a8739e999565f1fbbd6c5b44002a28048f55
unknown <arbyali217@gmail.com>
2024-12-26 21:56:41 +0700

    Praktikum 12

--- controllers/StudentControllers.js
+++ controllers/StudentControllers.js
@@ -1,18 +1,23 @@
-const students = require("../data/students");
+import students from "../data/students.js"; // Menggunakan import dengan ES6 syntax
 
 class StudentController {
-  // Menampilkan data  students
-  index(req, res) {
-    const data = {
-      message: "Menampilkan data students",
-      data: students,
-    };
-
-    res.json(data);
+  // Menampilkan data students
+  async index(req, res) {
+    try {
+      res.json({
+        message: "Menampilkan data students",
+        data: students,
+      });
+    } catch (err) {
+      res.status(500).json({
+        message: "Terjadi kesalahan saat mengambil data",
+        error: err.message,
+      });
+    }
   }
 
-  // Menambahkan data student baru
-  store(req, res) {
+  // Menambahkan data student
+  async store(req, res) {
     const { nama } = req.body;
 
     if (!nama) {
@@ -21,72 +26,68 @@ class StudentController {
       });
     }
 
-    const newStudent = {
-      id: students.length + 1,
-      nama,
-    };
-
-    students.push(newStudent); 
-
-    const data = {
-      message: `Menambahkan data student ${nama}`,
-      data: newStudent,
-    };
-
-    res.json(data);
-  }
-
-  destroy(req, res) {
-    const { id } = req.params;
-    const index = parseInt(id) - 1; 
-
-    if (index < 0 || index >= students.length) {
-      return res.status(404).json({
-        message: `Student dengan ID ${id} tidak ditemukan`,
+    try {
+      const newStudent = {
+        id: students.length + 1,
+        nama,
+      };
+      students.push(newStudent);
+      res.json({
+        message: `Menambahkan data student ${nama}`,
+        data: newStudent,
+      });
+    } catch (err) {
+      res.status(500).json({
+        message: "Terjadi kesalahan saat menambahkan data",
+        error: err.message,
       });
     }
-
-    const deletedStudent = students.splice(index, 1);
-
-    const data = {
-      message: `Menghapus data student dengan ID ${id}, dengan nama ${deletedStudent[0].nama}`,
-      data: students,
-    };
-
-    res.json(data);
   }
 
 
-  update(req, res) {
+  async destroy(req, res) {
+    const { id } = req.params;
+
+    try {
+      const index = parseInt(id) - 1;
+      const deletedStudent = students.splice(index, 1);
+      res.json({
+        message: `Menghapus data student dengan ID ${id}, dengan nama ${deletedStudent[0].nama}`,
+        data: students,
+      });
+    } catch (err) {
+      res.status(404).json({
+        message: err.message,
+      });
+    }
+  }
+
+  // Memperbarui data student berdasarkan ID
+  async update(req, res) {
     const { id } = req.params;
     const { nama } = req.body;
 
     if (!nama) {
       return res.status(400).json({
-        message: "Nama student harus diisi",
+        message: "Nama student harus diisi",  
       });
     }
 
-    const index = parseInt(id) - 1; 
-
-  
-    if (index < 0 || index >= students.length) {
-      return res.status(404).json({
-        message: `Student dengan ID ${id} tidak ditemukan`,
+    try {
+      const index = parseInt(id) - 1;
+      const oldName = students[index].nama;
+      students[index].nama = nama;
+      res.json({
+        message: `Memperbarui data student dengan ID ${id}, dari nama ${oldName} menjadi ${nama}`,
+        data: students[index],
+      });
+    } catch (err) {
+      res.status(404).json({
+        message: err.message,
       });
     }
-
-    const oldName = students[index].nama;
-    students[index].nama = nama; 
-
-    const data = {
-      message: `Memperbarui data student dengan ID ${id}, dari nama ${oldName} menjadi ${nama}`,
-      data: students[index],
-    };
-
-    res.json(data);
   }
 }
 
 const studentController = new StudentController();
-module.exports = studentController;
+export default studentController;
--- data/students.js
+++ data/students.js
@@ -1,5 +1,5 @@
-// TODO 1: Buat data students
+// data students
 const students = ["Arby", "Dika", "Ridho", "Ali"];
 
-// TODO 2: export data students
-module.exports = students;
+//  ES6 export
+export default students;

99d6fa0be33215b94d19cfcdab7e95d01cd79fdf
unknown <arbyali217@gmail.com>
2024-12-26 21:21:30 +0700

    Praktikum 11

--- controllers/StudentControllers.js
+++ controllers/StudentControllers.js
@@ -1,17 +1,17 @@
 const students = require("../data/students");
 
 class StudentController {
-  // Menampilkan data student
+  // Menampilkan data  students
   index(req, res) {
     const data = {
-      message: "Daftar data student",
+      message: "Menampilkan data students",
       data: students,
     };
 
     res.json(data);
   }
 
-  // Menambahkan data student
+  // Menambahkan data student baru
   store(req, res) {
     const { nama } = req.body;
 
@@ -23,66 +23,65 @@ class StudentController {
 
     const newStudent = {
       id: students.length + 1,
-      nama: nama,
+      nama,
     };
 
-    students.push(newStudent);
+    students.push(newStudent); 
 
     const data = {
-      message: `Student baru dengan nama ${nama} berhasil ditambahkan`,
+      message: `Menambahkan data student ${nama}`,
       data: newStudent,
     };
 
     res.json(data);
   }
 
-  // Menghapus data student berdasarkan ID
   destroy(req, res) {
     const { id } = req.params;
+    const index = parseInt(id) - 1; 
 
-    const studentIndex = students.findIndex((student) => student.id === parseInt(id));
-
-    if (studentIndex === -1) {
+    if (index < 0 || index >= students.length) {
       return res.status(404).json({
         message: `Student dengan ID ${id} tidak ditemukan`,
       });
     }
 
-    const deletedStudent = students.splice(studentIndex, 1);
+    const deletedStudent = students.splice(index, 1);
 
     const data = {
-      message: `Student dengan ID ${id} berhasil dihapus`,
-      data: deletedStudent,
+      message: `Menghapus data student dengan ID ${id}, dengan nama ${deletedStudent[0].nama}`,
+      data: students,
     };
 
     res.json(data);
   }
 
-  // Memperbarui data student berdasarkan ID
+
   update(req, res) {
     const { id } = req.params;
     const { nama } = req.body;
 
-    const studentIndex = students.findIndex((student) => student.id === parseInt(id));
+    if (!nama) {
+      return res.status(400).json({
+        message: "Nama student harus diisi",
+      });
+    }
 
-    if (studentIndex === -1) {
+    const index = parseInt(id) - 1; 
+
+  
+    if (index < 0 || index >= students.length) {
       return res.status(404).json({
         message: `Student dengan ID ${id} tidak ditemukan`,
       });
     }
 
-    if (!nama) {
-      return res.status(400).json({
-        message: "Nama baru tidak boleh kosong!",
-      });
-    }
-
-    const oldStudent = students[studentIndex];
-    students[studentIndex] = { id: oldStudent.id, nama: nama };
+    const oldName = students[index].nama;
+    students[index].nama = nama; 
 
     const data = {
-      message: `Data student dengan ID ${id} berhasil diperbarui`,
-      data: students[studentIndex],
+      message: `Memperbarui data student dengan ID ${id}, dari nama ${oldName} menjadi ${nama}`,
+      data: students[index],
     };
 
     res.json(data);
